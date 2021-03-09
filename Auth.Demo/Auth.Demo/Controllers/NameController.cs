f98b02463f8dee7322b69811657d080d423de27d
sajid <sajid.azhar@neurosensum.com>
2021-03-09 11:18:19 +0530

    restful api

---
+++ Auth.Demo/Auth.Demo/Controllers/NameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace Auth.Demo.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJWTAuthenticationManager jWTAuthenticationManager;
+
+        public NameController(IJWTAuthenticationManager jWTAuthenticationManager)
+        {
+            this.jWTAuthenticationManager = jWTAuthenticationManager;
+        }
+
+        // GET: api/Name
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "New York", "New Jersey" };
+        }
+
+        // GET: api/Name/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "New Jersey";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = jWTAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
---
+++ Auth.Demo/Auth.Demo/Controllers/UserCred.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Demo.Controllers
+{
+    public class UserCred
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
